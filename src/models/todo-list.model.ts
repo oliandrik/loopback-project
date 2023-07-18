e95c24c753ebbf54daf6d903c4514bbe57651fa0
oliandrik <os.shcherbakovaa@gmail.com>
2023-07-18 16:47:08 +0300

    first commit

---
+++ src/models/todo-list.model.ts
@@ -0,0 +1,27 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class TodoList extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  constructor(data?: Partial<TodoList>) {
+    super(data);
+  }
+}
+
+export interface TodoListRelations {
+  // describe navigational properties here
+}
+
+export type TodoListWithRelations = TodoList & TodoListRelations;
